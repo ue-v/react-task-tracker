7ecbb168afd1a9957552eacef5dfd8dcc90bec85
duy <tran.duy15@outlook.com>
2022-10-14 15:21:52 +0800

    rudimentary task tracker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  // Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Get Single task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    // if task id  === id then do change else keep as task
+    // get task to toggle
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(updatedTask),
+    });
+
+    const data = await res.json();
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !data.reminder } : task
+      )
+    );
+  };
+
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(task),
+    });
+
+    // data returned is new task added
+    const data = await res.json();
+    // get existing tasks array and add onto it data which is new task
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    //...tasks = copy task already there and then add new task
+    // setTasks([...tasks, newTask]);
+  };
+  // Delete Task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
+    setTasks(tasks.filter((task) => task.id !== id)); // how we deal with immutable "states"
+  };
+
+  // if task length greater than 0 ? show tasks else "no tasks to show"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No Tasks To Show"
+                )}
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/AddTask.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // validation for text
+    if (!text) {
+      alert("Please add a task");
+      return;
+    }
+
+    onAdd({ text, day, reminder });
+
+    setText("");
+    setDay("");
+    setReminder(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <lable>Task</lable>
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></input>
+      </div>
+      <div className="form-control">
+        <lable>Day & Time</lable>
+        <input
+          type="text"
+          placeholder="Add Day and Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        ></input>
+      </div>
+      <div className="form-control form-control-check">
+        <lable>Set Reminder</lable>
+        <input
+          type="checkbox"
+          checked={reminder}
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        ></input>
+      </div>
+
+      <input className="btn btn-block" type="submit" value="Save Task"></input>
+    </form>
+  );
+};
+
+export default AddTask;
